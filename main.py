57e55158f2f7e89a0e3aff5ea3028f3da50b50f6
swiftzeus <46224991+swiftzeus@users.noreply.github.com>
2021-08-29 10:04:10 +0100

    Almost there

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-print('Hello World!')
+#An attempt to replicate planetary motion
 
-x = 1
-y=5
 
-while x < y:
-    x+=1
-    print(x)
-
-print('Thanks that worked!')
-print('I think this is almost done')
