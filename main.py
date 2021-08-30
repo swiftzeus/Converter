2d538e42349ab6576a463dc4e9a4f58de5b488a9
swiftzeus <46224991+swiftzeus@users.noreply.github.com>
2021-08-30 20:09:42 +0100

    A change of direction - now working on gravitational project.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 
 G = 6.674 * 10**-11
 
-m1 = float(input('What is the mass of the first object? '))
-m2 = float(input('What is the mass of the second object? '))
-r = float(input('What is the distance between the objects? '))
+m1 = float(input('What is the mass of the first object in kilograms? '))
+m2 = float(input('What is the mass of the second object in kilograms? '))
+r = float(input('What is the distance between the objects in metres? '))
 
+Force = G * (m1 * m2)/r**2
+print(Force)
