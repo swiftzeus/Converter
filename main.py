79e453dee7ee16945c7258719e94feae57e3ff8b
swiftzeus <46224991+swiftzeus@users.noreply.github.com>
2021-08-30 20:07:00 +0100

    A change of direction - now working on gravitational project.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #An attempt to replicate planetary motion
 
 
+G = 6.674 * 10**-11
+
+m1 = float(input('What is the mass of the first object? '))
+m2 = float(input('What is the mass of the second object? '))
+r = float(input('What is the distance between the objects? '))
+
